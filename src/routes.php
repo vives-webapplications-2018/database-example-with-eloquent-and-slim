d4a2a40d67f6ad4e26b86ee1cc646728c8108143
Sille Van Landschoot <info@sillevl.be>
2018-10-19 16:29:06 +0200

    demo CRUD with the Todo model

--- src/routes.php
+++ src/routes.php
@@ -3,12 +3,37 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+use \App\Models\Todo;
+
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/todos', function (Request $request, Response $response, array $args) {
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+
+    // List all rows
+    $todos = Todo::all();
+
+    foreach ($todos as $todo) {
+        echo "<p>$todo->title: <br>$todo->description</p>";
+    }
+
+    // Get single row
+    $todo = Todo::find(4);
+
+    // Create new row
+    $todo = new Todo();
+    $todo->title = "Hello from php script";
+    $todo->description = "cool hé";
+    $todo->save();
+
+    // Edit existing row
+    $todo = Todo::find(3);
+    $todo->title = "new title";
+    $todo->save();
+
+    // Delete an row
+    $old = Todo::find(4);
+    $old->delete();
+
+    return $response;
 });
